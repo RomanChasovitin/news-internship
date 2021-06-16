99d3863f659090cc4c68053eea96eacfb7144f6a
Roman Chasovitin <chasovitin.r@gmail.com>
2021-06-16 13:01:08 +0600

    initla commit.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [news, setNews] = useState([])
+
+  const getNews = async () => {
+    const response = await fetch(`${process.env.REACT_APP_NEWS_API_URL}/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`)
+    const { articles } = await response.json()
+    console.log(articles)
+  }
+
+  useEffect(() => {
+    getNews()
+  })
+
   return (
     <div className="App">
       <header className="App-header">
