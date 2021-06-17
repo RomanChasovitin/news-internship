47856bf102586d8ade8ff6eec3783692d7ed1179
Spargwy <nameisconfidentialinformation@gmail.com>
2021-06-17 15:34:33 +0600

    Plugged in Roboto font, icons, added a topic in App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-// import { useEffect, useState } from 'react';
+import React from "react";
+import { ThemeProvider } from "@material-ui/core";
+import { unstable_createMuiStrictModeTheme } from "@material-ui/core/styles";
 
 function App() {
-  // const [news, setNews] = useState([])
-
-  // const getNews = async () => {
-  //   const response = await fetch(`${process.env.REACT_APP_NEWS_API_URL}/top-headlines?country=us&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`)
-  //   const { articles } = await response.json()
-  //   console.log(articles)
-  // }
-
-  // useEffect(() => {
-  //   getNews()
-  // })
-
+  const theme = unstable_createMuiStrictModeTheme();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>Hello</ThemeProvider>
     </div>
   );
 }
