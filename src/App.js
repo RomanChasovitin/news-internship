1f0f21578df7130a97e03cf6e601c33aaff54fa2
Spargwy <nameisconfidentialinformation@gmail.com>
2021-06-17 17:02:30 +0600

    put the theme variable in config theme.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { ThemeProvider } from "@material-ui/core";
-import { unstable_createMuiStrictModeTheme } from "@material-ui/core/styles";
+import { theme } from "./config/theme";
 
 function App() {
-  const theme = unstable_createMuiStrictModeTheme();
   return (
     <div className="App">
       <ThemeProvider theme={theme}>Hello</ThemeProvider>
