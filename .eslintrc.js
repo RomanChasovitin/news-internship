b07a893e53857915b476d14301fd350ab8a3317e
TangoPJ <livechess@yahoo.com>
2021-06-17 15:08:31 +0600

    update dependecies

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        printWidth: 120,
+        tabWidth: 2,
+        tabs: false,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
