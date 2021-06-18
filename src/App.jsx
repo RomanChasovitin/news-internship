048e2764295d2d44522a59642e40fcd5adcb9c76
Musa Aytmamatov <black.nobody.anb@gmail.com>
2021-06-18 14:55:40 +0600

    Added Sidebar component, separated styles in styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { ThemeProvider } from '@material-ui/core'
 import { theme } from './config/theme'
+import Sidebar from './components/core/Sidebar/Sidebar'
 
 function App() {
   return (
     <div className="App">
-      <ThemeProvider theme={theme}>Hello</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <Sidebar />
+      </ThemeProvider>
     </div>
   )
 }
