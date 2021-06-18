b3dc0fd2012860b929eaf04db5734b8d54a581a8
Musa Aytmamatov <black.nobody.anb@gmail.com>
2021-06-18 16:38:00 +0600

    I added a z-index to the header

--- src/components/core/Header/styles.js
+++ src/components/core/Header/styles.js
@@ -1,6 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
+  header: {
+    zIndex: 1500,
+  },
   searchButton: {
     margin: theme.spacing(1),
   },
