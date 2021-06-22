e8445d4eb29a3b3ef17b56dcf34f461bdbdd985d
Rail Batyrshin <lambda.zond@mail.ru>
2021-06-22 15:58:09 +0600

    Delete the slug from SearchPage

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
 
 // component
 import { Search } from '../../components/Search'
 
-const SearchPage = () => {
-  const { slug } = useParams()
-  return <Search type={slug} />
-}
+const SearchPage = () => <Search />
 
 export { SearchPage }
